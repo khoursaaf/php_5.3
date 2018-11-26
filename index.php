1473d5b2645af75f9feaa4bc47b7ee27603e8d21
khoursaaf <khoursa@gmail.com>
2018-11-26 16:44:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Avec le tableau de l'exercice , afficher la valeur de l'index 5.</p>
+      <?php
+      $months = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+       ?>
+      <p>La valeur de l'index 5 est : <?php echo $months[5]; ?>.</p>
+    </body>
+</html>
